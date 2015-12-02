ceefecf75b1c79a490a7abf5ca3460ac8dbd0976
Evgenii Muravev <evgenymuraviev@gmail.com>
2015-12-03 00:15:35 +0300

    Simplified the interface

--- app/controllers/MainController.scala
+++ app/controllers/MainController.scala
@@ -11,7 +11,6 @@ import play.api.libs.json.{JsValue, Json}
 import play.api.libs.streams.Streams
 import play.api.mvc.{Action, Controller}
 import stream.TwitterStreamListener
-import twitter4j.conf._
 
 
 object MainController extends Controller {
@@ -21,31 +20,17 @@ object MainController extends Controller {
     Streams.publisherToEnumerator(pubr)
   }
 
-  val cb = new ConfigurationBuilder()
-
-  cb.setDebugEnabled(true)
-    .setOAuthConsumerKey("jTWONyepVvv3M38h0xH2f04N2")
-    .setOAuthConsumerSecret("T6YjxKBiWSgFEgYnMfhOsE5UWUJps4VfHXn7GOroA4FcVCZldU")
-    .setOAuthAccessToken("597266731-z8p4D6SVyg5v5v2wQKbfXONPjRHxKn1FxVXe5UzF")
-    .setOAuthAccessTokenSecret("51HnIBEWfZ7gLLXmNNwwIRjIp6NARI5XW1sbMDAyKVc")
-
-  val config = cb.build
-  
-  def stream(query: String) = Action {
+  def stream() = Action {
 
     implicit val system = ActorSystem("mixedTweets")
     implicit val materializer = ActorFlowMaterializer()
 
     val toJson = (tweet: TweetInfo) => Json.obj("message" -> s"${tweet.searchQuery} : ${tweet.message}", "author" -> s"${tweet.author}")
 
-    val queries = query.split(",")
-
-    val streams = queries.map { query => 
-      val twitterStreamListener = new TwitterStreamListener(query, config)
-      twitterStreamListener.listenAndStream 
-    }
+    val streams = Seq(TwitterStreamListener.listenAndStream)
 
     val mergedStream = Source[TweetInfo]() { implicit builder =>
+      import FlowGraph.Implicits._
 
       val merge = builder.add(Merge[TweetInfo](streams.length))
 
@@ -58,17 +43,13 @@ object MainController extends Controller {
 
     val jsonStream = mergedStream.map(tweets => toJson(tweets))
 
-    val jsonEumerator : Enumerator[JsValue] = sourceToEnumerator(jsonStream)
+    val jsonEnumerator : Enumerator[JsValue] = sourceToEnumerator(jsonStream)
 
-    Ok.chunked(jsonEumerator through EventSource()).as("text/event-stream")  
+    Ok.chunked(jsonEnumerator through EventSource()).as("text/event-stream")
   } 
 
-  def liveTweets(query: List[String]) = Action {        
-    Ok(views.html.index(query))
-  }
-
-  def index = Action {
-    Redirect(routes.MainController.liveTweets(List("scala", "ruby")))
+  def index() = Action {
+    Ok(views.html.index())
   }
   
 }
--- app/stream/TwitterStreamListener.scala
+++ app/stream/TwitterStreamListener.scala
@@ -5,28 +5,34 @@ import akka.stream.scaladsl.{Keep, Sink, Source}
 import akka.stream.{ActorFlowMaterializer, OverflowStrategy}
 import model.TweetInfo
 import play.api.Logger
-import twitter4j._
-import twitter4j.conf.Configuration
-import twitter4j.{Status => TwitterStatus}
+import twitter4j.conf.ConfigurationBuilder
+import twitter4j.{Status => TwitterStatus, _}
 
 
-/**
- * @author Evgeniy Muravev
- */
-class TwitterStreamListener(searchQuery: String, config: Configuration) {
+object TwitterStreamListener {
 
   implicit val system = ActorSystem("mixedTweets")
   implicit val materializer = ActorFlowMaterializer()
 
-  val query = new FilterQuery(0, Array(), Array(searchQuery))
+  val searchQuery = "java"
+
+  val cb = new ConfigurationBuilder()
+
+  cb.setDebugEnabled(true)
+    .setOAuthConsumerKey("jTWONyepVvv3M38h0xH2f04N2")
+    .setOAuthConsumerSecret("T6YjxKBiWSgFEgYnMfhOsE5UWUJps4VfHXn7GOroA4FcVCZldU")
+    .setOAuthAccessToken("597266731-z8p4D6SVyg5v5v2wQKbfXONPjRHxKn1FxVXe5UzF")
+    .setOAuthAccessTokenSecret("51HnIBEWfZ7gLLXmNNwwIRjIp6NARI5XW1sbMDAyKVc")
+
+  val config = cb.build
+
+  val query = new FilterQuery(0, Array[Long](), Array(searchQuery))
 
   val twitterStream = new TwitterStreamFactory(config).getInstance
 
   def listenAndStream = {
-
     val (actorRef, publisher) = Source.actorRef[TweetInfo](1000, OverflowStrategy.fail).toMat(Sink.publisher)(Keep.both).run()
-
-    Logger.info(s"#start listener for $searchQuery")
+    Logger.info(s"Start listener for $searchQuery")
 
     val statusListener = new StatusAdapter() {
 
